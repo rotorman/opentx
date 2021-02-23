99c68408840b18583fc35b1c9fb973951ab90479
olliw42 <waldmanns@gmx.de>
2021-02-23 20:19:27 +0100

    improved xmavgenerate_dialect.py's

--- radio/src/thirdparty/Mavlink/fmavgenerate_dialect.py
+++ radio/src/thirdparty/Mavlink/fmavgenerate_dialect.py
@@ -4,12 +4,16 @@ fmavgenerate_dialect.py
 calls fastMavlink generator for C header files
 (c) olliw, olliw42
 '''
+import os
+import shutil
+import re
+import sys
+
 
 #options to set
 
-#mavlinkpathtorepository = r'C:\Users\Olli\Documents\GitHub\fastmavlink'
-#mavlinkpathtorepository = r'..\..\..\..\..\..\GitHub\fastmavlink'
-mavlinkpathtorepository = r'..\..\..\..\..\fastmavlink'
+#mavlinkpathtorepository = os.path.join("C:/",'Users','Olli','Documents','GitHub','fastmavlink')
+mavlinkpathtorepository = os.path.join('..','..','..','..','..','fastmavlink')
 
 #mavlinkdialect = "\..\mavlink\external\dialects\storm32"
 mavlinkdialect = "opentx"
@@ -21,11 +25,6 @@ mavlinkoutputdirectory = 'out'
 '''
 Imports
 '''
-import os
-import shutil
-import re
-import sys
-
 #import pkgutil
 #search_path = [mavlinkpathtorepository] # set to None to see all modules importable from sys.path
 #all_modules = [x[1] for x in pkgutil.iter_modules(path=search_path)]
@@ -42,29 +41,28 @@ Generates the header files and place them in the output directory.
 '''
 
 outdir = mavlinkoutputdirectory
-        
-#xmfile = os.path.abspath(mavlinkpathtorepository + mavlinkdialect+'.xml')
+
+#xmfile = os.path.abspath(os.path.join(mavlinkpathtorepository, mavlinkdialect+'.xml'))
 xmfile = mavlinkdialect+'.xml'
 
 #recreate out directory
 print('----------')
 print('kill out dir')
 try:
-    shutil.rmtree(outdir)
+shutil.rmtree(outdir)
 except:
-    pass    
+pass    
 os.mkdir(outdir)
 print('----------')
 
-if True:
-        opts = fmavgen.Opts(outdir, parse_flags=fmavflags.PARSE_FLAGS_WARNING_ENUM_VALUE_MISSING)
-        args = [xmfile]
-        try:
-            fmavgen.fmavgen(opts,args)
-            print('Successfully Generated Headers', 'Headers generated successfully.')
+opts = fmavgen.Opts(outdir, parse_flags=fmavflags.PARSE_FLAGS_WARNING_ENUM_VALUE_MISSING)
+args = [xmfile]
+try:
+fmavgen.fmavgen(opts,args)
+print('Successfully Generated Headers', 'Headers generated successfully.')
 
-        except Exception as ex:
-            exStr = str(ex)
-            print('Error Generating Headers','{0!s}'.format(exStr))
-            exit()
+except Exception as ex:
+exStr = str(ex)
+print('Error Generating Headers','{0!s}'.format(exStr))
+exit()
 
--- radio/src/thirdparty/Mavlink/pymavgenerate_dialect.py
+++ radio/src/thirdparty/Mavlink/pymavgenerate_dialect.py
@@ -4,51 +4,49 @@ mavgenerate_dialect.py
 calls mavlink generator for C header files
 (c) olliw, olliw42
 '''
+import os
+import shutil
+import re
+import sys
+
 
 #options to set
+
+mavlinkpathtorepository = os.path.join('mavlink')
 
 #mavlinkdialect = "storm32"
 mavlinkdialect = "opentx"
 
 mavlinkoutputdirectory = 'out'
 
-mavlinkpathtorepository = r'mavlink'
-
-
-mavlinklight = False
-#mavlinklight = True
-
 
 '''
 Imports
 '''
-import os
-import shutil
-import re
-import sys
+#import pkgutil
+#search_path = [mavlinkpathtorepository] # set to None to see all modules importable from sys.path
+#all_modules = [x[1] for x in pkgutil.iter_modules(path=search_path)]
+#print(all_modules)
 
 #we may have not installed it or have different pymavlink, so set things straight
 sys.path.insert(0,mavlinkpathtorepository)
 
-if mavlinklight:
-    from pymavlink_light.generator import mavgen
-    from pymavlink_light.generator import mavparse
-else:
-    from pymavlink.generator import mavgen
-    from pymavlink.generator import mavparse
+from pymavlink.generator import mavgen
+from pymavlink.generator import mavparse
 
 '''
 Generates the header files and place them in the output directory.
 '''
 
 outdir = mavlinkoutputdirectory
+
+xmfile = mavlinkdialect+'.xml'
+
 wire_protocol = mavparse.PROTOCOL_2_0
 language = 'C'
 validate = mavgen.DEFAULT_VALIDATE
 error_limit = 5
 strict_units = mavgen.DEFAULT_STRICT_UNITS
-        
-xmfile = mavlinkdialect+'.xml'        
 
 #recreate out directory
 print('----------')
@@ -56,19 +54,18 @@ print('kill out dir')
 try:
     shutil.rmtree(outdir)
 except:
-    pass    
+    pass
 os.mkdir(outdir)
 print('----------')
 
-if True:
-        opts = mavgen.Opts(outdir, wire_protocol=wire_protocol, language=language, validate=validate, error_limit=error_limit, strict_units=strict_units)
-        args = [xmfile]
-        try:
-            mavgen.mavgen(opts,args)
-            print('Successfully Generated Headers', 'Headers generated successfully.')
+opts = mavgen.Opts(outdir, wire_protocol=wire_protocol, language=language, validate=validate, error_limit=error_limit, strict_units=strict_units)
+args = [xmfile]
+try:
+    mavgen.mavgen(opts,args)
+    print('Successfully Generated Headers', 'Headers generated successfully.')
 
-        except Exception as ex:
-            exStr = str(ex)
-            print('Error Generating Headers','{0!s}'.format(exStr))
-            exit()
+except Exception as ex:
+    exStr = str(ex)
+    print('Error Generating Headers','{0!s}'.format(exStr))
+    exit()
 
